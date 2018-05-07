7cd32c59f6dd75f3ec45fc16edbc261e3fadfdd4
healim <healim.me@gmail.com>
2018-05-08 02:08:20 +0900

    main, q-card(default) layout test

--- src/app.py
+++ src/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('main.html')
+
+@app.route('/q-card')
+def qcard():
+    return render_template('q-card.html')
 
 if __name__ == '__main__':
     app.run(port=4992, use_reloader=True, debug=True)
